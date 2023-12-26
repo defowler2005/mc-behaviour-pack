16a9af4c0b78260a07238b195ee4e9f3be5d3170
defowler2005 <defowler2005@gmail.com>
2023-12-27 08:44:21 +1030

    ip

--- scripts/example/commands/staff/gui.js
+++ scripts/example/commands/staff/gui.js
@@ -2,13 +2,31 @@ import { world } from '@minecraft/server';
 import { commandBuilder } from '../../../library/build/classes/commandBuilder.js';
 
 /**
- * Array of staff modules.
+ * Array of modules for both staff and players.
  * @type {Array<Object>}
+ * @property {String} displayName - The string that the player will see.
+ * @property {String} moduleId - The string to be used for the dynamic property.
+ * @property {Number} indexId - The number for the index of the module.
+ * @property {String} role - The role to indicate what modle is for who.
  */
-export const staffModules = [
+export const modulesList = [
     {
-        displayName: '',
-        moduleId: '',
-        indexId: 0
+        displayName: 'Chat ranks',
+        moduleId: 'chat_ranks',
+        role: 'staff',
+        indexId: 0,
     }
 ];
+
+/**
+ * The GUI object containing all menus.
+ * @type {Object}
+ * @property {Object} staff - The staff menu object.
+ * @property {Object} player - The player menu object.
+ * @property {Object} welcome - The welcome menu object.
+ */
+const gui = {
+    staff: {},
+    player: {},
+    welcome: {}
+};
