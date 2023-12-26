cb1cfd8a70ba0be718ee600a37cf0ed1c219f580
defowler2005 <defowler2005@gmail.com>
2023-12-27 08:05:41 +1030

    d

---
+++ scripts/example/commands/staff/gui.js
@@ -0,0 +1,14 @@
+import { world } from '@minecraft/server';
+import { commandBuilder } from '../../../library/build/classes/commandBuilder.js';
+
+/**
+ * Array of staff modules.
+ * @type {Array<Object>}
+ */
+export const staffModules = [
+    {
+        displayName: '',
+        moduleId: '',
+        indexId: 0
+    }
+];
