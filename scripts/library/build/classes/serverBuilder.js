14e41b75e46b11b8a6c88e2e8731abb908e0a1b2
defowler2005 <defowler2005@gmail.com>
2024-03-15 11:28:02 +1100

    efveefc

--- scripts/library/build/classes/serverBuilder.js
+++ scripts/library/build/classes/serverBuilder.js
@@ -1,7 +1,15 @@
-import { world } from "@minecraft/server";
-
+import { system, world } from "@minecraft/server";
+import { configurations } from '../configurations.js';
+/**
+ * A class for performing server related stuff.
+ * @class
+ */
 export class serverBuilder {
     constructor() {
-
-    };
+        system.runInterval(() => {
+            this.allStaffPlayers = world.getPlayers({ tags: [configurations.staffTag] });
+            this.allNonStaffPlayers = world.getPlayers({ excludeTags: [configurations.staffTag] });
+            this.allEntities = world.getDimension('overworld').getEntities({ excludeTypes: ['minecraft:player'] });
+        })
+    }
 };
