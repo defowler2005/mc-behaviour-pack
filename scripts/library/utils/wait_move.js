0e1937c8e16a0b4a0b46b152444febb08eaac734
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2024-11-26 10:32:07 +1100

    Update wait_move.js

--- scripts/library/utils/wait_move.js
+++ scripts/library/utils/wait_move.js
@@ -1,24 +1,21 @@
-import { system } from "@minecraft/server";
+import { Player, system } from '@minecraft/server';
 
 /**
  * Waits for the player to move and then executes a callback function.
- * @param {Object} target - The target player to monitor for movement.
+ * @param {Player} target - The target player to monitor for movement.
  * @param {Number} x - The initial X-coordinate of the target player.
  * @param {Number} y - The initial Y-coordinate of the target player.
  * @param {Number} z - The initial Z-coordinate of the target player.
  * @param {Function} callback - The callback function to execute after the player moves.
  */
 
-export function waitMove(target, { x, y, z }, callback) {
-    const t = new Map();
-    t.set(target, [x, y, z]);
+export function waitMove(target, x, y, z, callback) {
+    const map1 = new Map();
+    map1.set(target, [x, y, z]);
     system.runInterval(() => {
-        for (const [target, [xOld, yOld, zOld]] of t) {
+        for (const [target, [xOld, yOld, zOld]] of map1) {
             const { x: xc, y: yc, z: zc } = target.location;
-            if (xOld !== xc || yOld !== yc || zOld !== zc) system.run(() => {
-                t.delete(target);
-                callback({ initial_location: { x: xOld, y: yOld, z: zOld } });
-            });
+            if (xOld !== xc || yOld !== yc || zOld !== zc) system.run(() => { map1.delete(target); callback() });
         }
     }, 2);
 };
